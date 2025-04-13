110608dd66baaf0586709c92ccda9cc811d10ed6
Andy Madrigal <andymadrigal@estudiantec.cr>
2025-04-12 21:10:17 -0600

    menu section updated

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -4,13 +4,7 @@ import './Footer.css';
 const Footer: React.FC = () => {
     return (
         <footer className="footer">
-            <p>&copy; 2025 Blue Mesito. Todos los derechos reservados</p>
-            <div className="footer__links">
-                <a href="https://www.deviantart.com/bluemesito/gallery" target="_blank" rel="noreferrer">DevianArt</a>
-                <a href="https://www.pixiv.net/en/users/19455541" target="_blank" rel="noreferrer">Pixiv</a>
-                <a href="https://www.instagram.com/bluemesito" target="_blank" rel="noreferrer">Instagram</a>
-                <a href="https://x.com/bluemesito" target="_blank" rel="noreferrer">X</a>
-            </div>
+            <p>&copy; 2025 BlueMesito. Todos los Derechos Reservados</p>
         </footer>
     );
 };
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Header.css';
 import main_logo from '../Assets/main_logo.png'; // Import the logo file
+import { ReactComponent as DevianArtIcon } from '../Assets/deviantart.svg';
+import { ReactComponent as InstagramIcon } from '../Assets/instagram.svg';
+import { ReactComponent as PixivIcon } from '../Assets/pixiv.svg';
 
 const Header: React.FC = () => {
     return (
@@ -19,6 +22,20 @@ const Header: React.FC = () => {
                 <a href="#contact" className="header__link">CONTACT</a>
                 <a href="#about" className="header__link">ABOUT</a>
             </nav>
+
+            {/* Social Media Icons */}
+            <div className="header__social-media">
+                <a href="https://www.deviantart.com/bluemesito/gallery" target="_blank" rel="noopener noreferrer" className="header__social-link">
+                    <DevianArtIcon className="header__social-icon" />
+                </a>
+                <a href="https://www.instagram.com/bluemesito" target="_blank" rel="noopener noreferrer" className="header__social-link">
+                    <InstagramIcon className="header__social-icon" />
+                </a>
+                <a href="https://www.pixiv.net/en/users/19455541" target="_blank" rel="noopener noreferrer" className="header__social-link">
+                    <PixivIcon className="header__social-icon" />
+                </a>
+            </div>
+
         </header>
     );
 };
